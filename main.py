9671fd2eac9015a94d6913a159c93ee5a004bc6e
Abrahim Zaman A <46282325+abrahimzaman@users.noreply.github.com>
2021-02-23 09:49:31 +0500

    Updated Main.py
    Added New Exception Blockers

--- main.py
+++ main.py
@@ -1,12 +1,24 @@
+#Author: Abrahim Zaman A.
+#Date:   21.02.2021.
+#Programming Language: Python.
+#Last Updated: 23.02.2021.
+#MIT Licensed.
+#Version: 1.0.1
+#Libraries: Selenium, ChromeWebDrivers
+
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 
-my_links_clicked = input("Enter how much links you wanna open!")
+my_links_clicked = input("Enter how much links you wanna open! \n")
 latest = int(my_links_clicked)
-my_link_to_be_click = str(input("Enter Link Address!"))
+my_link_to_be_click = str(input("Enter Link Address! \n"))
 for i in range(1, latest):
-    driver = webdriver.Chrome()
-    driver.get(my_link_to_be_click)
-    driver.find_element_by_tag_name('body').send_keys(Keys.CONTROL + 't')
-    driver.get(my_link_to_be_click)
-    driver.close()
+	try:
+		driver = webdriver.Chrome()
+		driver.get(my_link_to_be_click)
+		driver.find_element_by_tag_name('body').send_keys(Keys.CONTROL + 't')
+		driver.get(my_link_to_be_click)
+		driver.close()
+	except Exception:
+		print("Error!")
+		break
